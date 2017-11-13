74b5880e18300ba5531a77d803608c4b228b62a8
Sérgio Ramos <mail@sergioramos.me>
2017-11-13 15:54:50 +0000

    first commit

---
+++ README.md
@@ -0,0 +1,45 @@
+# declarative-redux-form
+
+[![npm](https://img.shields.io/npm/v/declarative-redux-form.svg?style=flat-square)](https://www.npmjs.com/package/declarative-redux-form)
+[![License: BSD 3-clause "New" or "Revised" License](https://img.shields.io/badge/License-MPL%202.0-brightgreen.svg?style=flat-square)](https://opensource.org/licenses/BSD-3-Clause)
+[![standard-readme compliant](https://img.shields.io/badge/standard--readme-OK-green.svg?style=flat-square)](https://github.com/RichardLitt/standard-readme)
+
+[![David](https://img.shields.io/david/ramitos/declarative-redux-form.svg?style=flat-square)](https://david-dm.org/ramitos/declarative-redux-form)
+[![David](https://img.shields.io/david/dev/ramitos/declarative-redux-form.svg?style=flat-square)](https://david-dm.org/ramitos/declarative-redux-form?type=dev)
+[![David](https://img.shields.io/david/peer/ramitos/declarative-redux-form.svg?style=flat-square)](https://david-dm.org/ramitos/declarative-redux-form?type=peer)
+
+Declarative [redux-form](https://redux-form.com).
+
+## Table of Contents
+
+* [Install](#install)
+* [Usage](#usage)
+* [License](#license)
+
+## Install
+
+```
+yarn add --dev declarative-redux-form
+```
+
+## Usage
+
+```js
+import ReduxForm from 'declarative-redux-form';
+import { Field } from 'redux-form';
+
+export default () => (
+  <ReduxForm form="form-name">
+    {({ handleSubmit }) => (
+      <form onSubmit={handleSubmit}>
+        <Field name="name" type="text" />
+        <button type="submit" />
+      </form>
+    )}
+  </ReduxForm>
+);
+```
+
+## License
+
+BSD-3-Clause
---
+++ src/index.js
@@ -0,0 +1,91 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { reduxForm } from 'redux-form';
+import isEqual from 'lodash.isequal';
+
+const fields = {
+  asyncBlurFields: PropTypes.arrayOf(PropTypes.string),
+  destroyOnUnmount: PropTypes.bool,
+  forceUnregisterOnUnmount: PropTypes.bool,
+  enableReinitialize: PropTypes.bool,
+  keepDirtyOnReinitialize: PropTypes.bool,
+  form: PropTypes.string.required,
+  initialValues: PropTypes.object,
+  getFormState: PropTypes.func,
+  onChange: PropTypes.func,
+  onSubmit: PropTypes.func,
+  onSubmitFail: PropTypes.func,
+  onSubmitSuccess: PropTypes.func,
+  propNamespace: PropTypes.string,
+  validate: PropTypes.func,
+  warn: PropTypes.func,
+  touchOnBlur: PropTypes.bool,
+  touchOnChange: PropTypes.bool,
+  persistentSubmitErrors: PropTypes.bool,
+  registeredFields: PropTypes.any
+};
+
+const fieldKeys = Object.keys(fields);
+
+export default class ReduxForm extends Component {
+  constructor(args) {
+    super(args);
+
+    this.state = {
+      Form: ReduxForm.createReduxForm(this.props)(this.renderForm)
+    };
+  }
+
+  static didPropsChange = (prevProps, nextProps) =>
+    fieldKeys.some(name => !isEqual(prevProps[name], nextProps[name]));
+
+  static getReduxFormProps = props =>
+    fieldKeys.reduce(
+      (all, name) => ({
+        ...all,
+        [name]: props[name]
+      }),
+      {}
+    );
+
+  static createReduxForm = props =>
+    reduxForm(ReduxForm.getReduxFormProps(props));
+
+  static cleanReduxFormProps = props =>
+    Object.keys(props)
+      .filter(name => fieldKeys.indexOf(name) < 0)
+      .reduce(
+        (all, name) => ({
+          ...all,
+          [name]: props[name]
+        }),
+        {}
+      );
+
+  componentWillReceiveProps(nextProps) {
+    if (ReduxForm.didPropsChange(this.props, nextProps)) {
+      return;
+    }
+
+    this.setState({
+      Form: ReduxForm.createReduxForm(this.props)(this.renderForm)
+    });
+  }
+
+  renderForm = childProps => {
+    const { children } = this.props;
+    return children(childProps);
+  };
+
+  render = () => {
+    const { parentProps } = ReduxForm.cleanReduxFormProps(this.props);
+    const { Form } = this.state;
+
+    return React.createElement(Form, parentProps);
+  };
+}
+
+ReduxForm.propTypes = {
+  children: PropTypes.func.required,
+  ...fields
+};
